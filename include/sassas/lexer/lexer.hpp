7c5327edd4f8c6b37333c7d4b3e6fd95f96ed624
ZPlutoY <38772646+Pluto-Zy@users.noreply.github.com>
2025-05-07 21:38:57 +0800

    [Lexer] Implement `lex_until` method for flexible token lexing based on conditions.

--- include/sassas/lexer/lexer.hpp
+++ include/sassas/lexer/lexer.hpp
@@ -3,6 +3,8 @@
 
 #include "sassas/lexer/token.hpp"
 
+#include <concepts>
+#include <functional>
 #include <iterator>
 #include <string_view>
 
@@ -23,6 +25,35 @@ public:
     /// `current_` to the end of the token. The generated token is stored in `cur_token_` and
     /// returned by this function.
     auto next_token() -> Token const &;
+
+    /// Lexes until the condition `cond` is satisfied. `cond` is a callable object that takes a
+    /// `Token` object as an argument and returns a boolean value. The `consume` parameter indicates
+    /// whether to consume this token. If a token that satisfies the condition is encountered, it
+    /// returns `true`, otherwise it returns `false` (i.e., all tokens are consumed).
+    template <class Fn>
+        requires std::copy_constructible<Fn> && std::predicate<Fn &, Token const &>
+    auto lex_until(Fn cond, bool consume) -> bool {
+        while (cur_token_.is_not(Token::End) && !std::invoke(cond, cur_token_)) {
+            next_token();
+        }
+
+        if (cur_token_.is(Token::End)) {
+            return false;
+        }
+
+        if (consume) {
+            next_token();
+        }
+
+        return true;
+    }
+
+    /// Lexes until a token of type `kind` is encountered. The `consume` parameter indicates whether
+    /// to consume this token. If a token of type `kind` is encountered, it returns `true`,
+    /// otherwise it returns `false` (i.e., all tokens are consumed).
+    auto lex_until(Token::TokenKind kind, bool consume) -> bool {
+        return lex_until([kind](Token const &token) { return token.is(kind); }, consume);
+    }
 
 private:
     /// The content of the source code.
