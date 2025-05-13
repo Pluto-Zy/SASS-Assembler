50961e67ff51e7dfeee5f1d2d7bb842a30894b44
ZPlutoY <38772646+Pluto-Zy@users.noreply.github.com>
2025-05-13 14:54:32 +0800

    [Parser] Refactor the implementation of `get_integer_constant()`. Use the `IntegerParser` class to handle integer parsing and error diagnostics. Enhance the logic for handling integer overflow.

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -2,6 +2,7 @@
 
 #include "sassas/diagnostic/diagnostic.hpp"
 #include "sassas/lexer/token.hpp"
+#include "sassas/utils/unreachable.hpp"
 
 #include "fmt/format.h"
 
@@ -15,6 +16,7 @@
 #include <cstdint>
 #include <iterator>
 #include <optional>
+#include <string>
 #include <string_view>
 #include <utility>
 
@@ -94,218 +96,317 @@ auto Parser::expect_string_literal(Token const &token) -> std::optional<std::str
     }
 }
 
-auto Parser::get_integer_constant(Token const &token, unsigned bits, bool signedness)
-    -> std::optional<std::uint64_t>  //
-{
-    assert(token.is(Token::Integer) && "Expected an integer constant token");
-    assert(bits > 0 && bits <= 64 && "Expected a positive integer less than or equal to 64");
+namespace {
+/// This is a parser for integer constants. It is used to parse integer constants in the source code
+/// and convert them into a `std::uint64_t` value. The parser handles various formats, including
+/// decimal, hexadecimal, and binary. It also supports signed and unsigned integers, as well as
+/// integer constants with digit separators (e.g., `1_000`).
+class IntegerParser {
+public:
+    struct DiagMessage {
+        unsigned annotation_begin;
+        unsigned annotation_end;
+        std::string message;
+    };
 
-    std::string_view content = token.content();
-    assert(!content.empty() && "Expected a non-empty integer constant token");
+    explicit IntegerParser(Token const &token, unsigned bits, bool signedness) :
+        content_(token.content()),
+        bits_(bits),
+        signedness_(signedness),
+        cur_pos_(token.location()),
+        negative_(false),
+        base_(10)  //
+    {
+        assert(token.is(Token::Integer) && "Expected an integer constant token");
+        assert(bits > 0 && bits <= 64 && "Expected a positive integer less than or equal to 64");
+        assert(!content_.empty() && "Expected a non-empty integer constant token");
+    }
 
-    // This value records the actual position of the character we are currently processing in the
-    // token.
-    std::size_t cur_pos = token.location();
+    /// Parses the sign of the integer constant. Stores the sign in the `negative_` member.
+    void parse_sign() {
+        if (content_.front() == '-') {
+            content_.remove_prefix(1);
+            ++cur_pos_;
 
-    // Determine the sign of the integer constant.
-    bool const negative = [&] {
-        if (content.front() == '-') {
-            content.remove_prefix(1);
-            ++cur_pos;
-            return true;
-        } else if (content.front() == '+') {
-            content.remove_prefix(1);
-            ++cur_pos;
-            return false;
-        } else {
-            return false;
+            negative_ = true;
+        } else if (content_.front() == '+') {
+            content_.remove_prefix(1);
+            ++cur_pos_;
         }
-    }();
-    assert((signedness || !negative) && "`negative` can only be set for signed integers");
 
-    // Remove all whitespace before the sign and the first integer digit, because the sign and
-    // integer may be concatenated from two tokens.
-    std::size_t const while_space_count = std::ranges::distance(
-        content.begin(),
-        std::ranges::find_if_not(content, static_cast<int (*)(int)>(std::isspace))
-    );
-    content.remove_prefix(while_space_count);
-    cur_pos += while_space_count;
-    assert(!content.empty() && "Expected a non-empty integer constant token");
+        assert((signedness_ || !negative_) && "`negative_` can only be set for signed integers");
 
-    // Determine the base of the integer constant.
-    unsigned const base = [&] {
+        // Remove all whitespace before the sign and the first integer digit, because the sign and
+        // integer may be concatenated from two tokens.
+        std::size_t const while_space_count = std::ranges::distance(
+            content_.begin(),
+            std::ranges::find_if_not(content_, static_cast<int (*)(int)>(std::isspace))
+        );
+
+        content_.remove_prefix(while_space_count);
+        cur_pos_ += while_space_count;
+        assert(!content_.empty() && "Expected a non-empty integer constant token");
+    }
+
+    /// Parses the base of the integer constant. Stores the base in the `base_` member.
+    void parse_base() {
         // The lexer implementation ensures that the first character of this token is an integer.
-        if (content.front() == '0') {
-            content.remove_prefix(1);
-            ++cur_pos;
+        if (content_.front() == '0') {
+            content_.remove_prefix(1);
+            ++cur_pos_;
 
-            if (!content.empty()) {
-                switch (content.front()) {
+            if (!content_.empty()) {
+                switch (content_.front()) {
                 case 'b':
                 case 'B':
-                    content.remove_prefix(1);
-                    ++cur_pos;
-                    return 2;
+                    content_.remove_prefix(1);
+                    ++cur_pos_;
+
+                    base_ = 2;
+                    break;
 
                 case 'x':
                 case 'X':
-                    content.remove_prefix(1);
-                    ++cur_pos;
-                    return 16;
+                    content_.remove_prefix(1);
+                    ++cur_pos_;
+
+                    base_ = 16;
+                    break;
 
                 default:
+                    base_ = 8;
                     break;
                 }
             }
-
-            return 8;
-        } else {
-            return 10;
         }
-    }();
+    }
 
-    // Determine the value range of the integer constant.
-    auto const [lb, ub] = [&] {
-        if (signedness) {
-            // The range of signed integers is [-2^(bits - 1), 2^(bits - 1) - 1].
-            std::uint64_t const max = (static_cast<std::uint64_t>(1) << (bits - 1)) - 1;
-            std::uint64_t const min = -(max + 1);
-            return std::pair(min, max);
-        } else {
-            // The range of unsigned integers is [0, 2^bits - 1].
-            std::uint64_t const max = (static_cast<std::uint64_t>(1) << bits) - 1;
-            return std::pair(static_cast<std::uint64_t>(0), max);
-        }
-    }();
+    /// Checks the validity of digit separators in the integer constant.
+    auto check_separator() const -> std::optional<DiagMessage> {
+        for (std::size_t i = 0; i != content_.size(); ++i) {
+            char const ch = content_[i];
 
-    // The result of the integer constant.
-    std::uint64_t result = 0;
-
-    // Helper function that checks whether the integer constant is within the valid range.
-    auto const check_overflow = [&] {
-        if (signedness) {
-            auto const target = static_cast<std::int64_t>(negative ? -result : result);
-            auto const lower_bound = static_cast<std::int64_t>(lb);
-            auto const upper_bound = static_cast<std::int64_t>(ub);
-
-            return lower_bound <= target && target <= upper_bound;
-        } else {
-            return result <= ub;
-        }
-    };
-
-    Diag diag = create_diag_at_token(token, DiagLevel::Error, "Invalid integer constant");
-
-    // A RAII class to manage the lifetime of the diagnostic object. It ensures that the diagnostic
-    // object is added to the `diagnostics_` vector when it goes out of scope.
-    struct AddDiagRAII {
-        explicit AddDiagRAII(Diag &diag, Parser &self) : diag_(&diag), self_(self) { }
-        ~AddDiagRAII() {
-            if (diag_) {
-                self_.diagnostics_.push_back(std::move(*diag_));
+            if (!is_separator(ch)) {
+                continue;
             }
-        }
 
-        void release() {
-            diag_ = nullptr;
-        }
-
-    private:
-        Diag *diag_;
-        Parser &self_;
-    } add_diag_raii(diag, *this);
-
-    for (std::size_t i = 0; i != content.size(); ++i) {
-        char const ch = content[i];
-
-        if (ch == '_') {
-            // The digit separator '_' cannot appear at the beginning and end.
-            if (i == 0 || i == content.size() - 1) {
-                auto note =
-                    ants::DiagEntry(DiagLevel::Note, "because the digit separator cannot be here")
-                        .with_source(
-                            ants::AnnotatedSource(lexer_.source(), origin_)
-                                .with_primary_annotation(cur_pos + i, cur_pos + i + 1)
-                        );
-
-                diag.add_sub_diag_entry(std::move(note));
-                return std::nullopt;
+            // The digit separator cannot appear at the beginning and end.
+            if (i == 0 || i == content_.size() - 1) {
+                return DiagMessage {
+                    .annotation_begin = static_cast<unsigned>(cur_pos_ + i),
+                    .annotation_end = static_cast<unsigned>(cur_pos_ + i + 1),
+                    .message = "because the digit separator cannot be here",
+                };
             }
 
             // Cannot use consecutive digit separators.
-            if (content[i - 1] == '_') {
-                auto note =  //
-                    ants::DiagEntry(
-                        DiagLevel::Note,
-                        "because the digit separator cannot be used consecutively"
-                    )
-                        .with_source(
-                            ants::AnnotatedSource(lexer_.source(), origin_)
-                                .with_primary_annotation(cur_pos + i - 1, cur_pos + i + 1)
-                        );
-
-                diag.add_sub_diag_entry(std::move(note));
-                return std::nullopt;
-            }
-
-            // In other cases, we can ignore the digit separator.
-            continue;
-        }
-
-        // Check the validity of the digit.
-        unsigned digit;
-        if ('0' <= ch && ch <= std::ranges::min('9', static_cast<char>('0' + base))) {
-            digit = ch - '0';
-        } else if ('a' <= ch && ch <= std::ranges::min('f', static_cast<char>('a' + base - 10))) {
-            digit = ch - 'a' + 10;
-        } else if ('A' <= ch && ch <= std::ranges::min('F', static_cast<char>('A' + base - 10))) {
-            digit = ch - 'A' + 10;
-        } else {
-            string_pool_.push_back(
-                fmt::format("because this is not a valid digit in base {}", base)
-            );
-
-            auto note =  //
-                ants::DiagEntry(DiagLevel::Note, string_pool_.back())
-                    .with_source(
-                        ants::AnnotatedSource(lexer_.source(), origin_)
-                            .with_primary_annotation(cur_pos + i, cur_pos + i + 1)
-                    );
-
-            diag.add_sub_diag_entry(std::move(note));
-            return std::nullopt;
-        }
-
-        std::uint64_t const value = result * base + digit;
-        if (value >= result) {
-            result = value;
-
-            if (check_overflow()) {
-                continue;
+            if (is_separator(content_[i - 1])) {
+                return DiagMessage {
+                    .annotation_begin = static_cast<unsigned>(cur_pos_ + i - 1),
+                    .annotation_end = static_cast<unsigned>(cur_pos_ + i + 1),
+                    .message = "because the digit separator cannot be used consecutively",
+                };
             }
         }
 
-        // If we reach here, it means the integer constant has overflowed.
-        diag.add_sub_diag_entry(DiagLevel::Note, "because the integer constant overflows");
-        if (signedness) {
-            string_pool_.push_back(
-                fmt::format(
-                    "the valid range is [{}, {}]",
-                    static_cast<std::int64_t>(lb),
-                    static_cast<std::int64_t>(ub)
-                )
-            );
-        } else {
-            string_pool_.push_back(fmt::format("the valid range is [{}, {}]", lb, ub));
-        }
-
-        diag.add_sub_diag_entry(DiagLevel::Note, string_pool_.back());
         return std::nullopt;
     }
 
-    add_diag_raii.release();
-    return negative ? -result : result;
+    /// Checks the validity of the digit in the integer constant.
+    auto check_digit() const -> std::optional<DiagMessage> {
+        for (std::size_t i = 0; i != content_.size(); ++i) {
+            char const ch = content_[i];
+            if (is_separator(ch)) {
+                continue;
+            }
+
+            if ((ch < '0' || ch > std::ranges::min('9', static_cast<char>('0' + base_)))
+                && (ch < 'a' || ch > std::ranges::min('f', static_cast<char>('a' + base_ - 10)))
+                && (ch < 'A' || ch > std::ranges::min('F', static_cast<char>('A' + base_ - 10))))
+            {
+                return DiagMessage {
+                    .annotation_begin = static_cast<unsigned>(cur_pos_ + i),
+                    .annotation_end = static_cast<unsigned>(cur_pos_ + i + 1),
+                    .message = fmt::format("because this is not a valid digit in base {}", base_),
+                };
+            }
+        }
+
+        return std::nullopt;
+    }
+
+    /// Parses the integer constant and returns its value as a `std::uint64_t`. If the integer
+    /// constant overflows, returns `std::nullopt`.
+    auto parse_integer() const -> std::optional<std::uint64_t> {
+        std::uint64_t const max_value = get_max_value();
+        std::uint64_t result = 0;
+
+        if (always_fits_into_64bits()) {
+            for (char const ch : content_) {
+                if (is_separator(ch)) {
+                    continue;
+                }
+
+                result = result * base_ + digit_value(ch);
+            }
+        } else {
+            for (char const ch : content_) {
+                if (is_separator(ch)) {
+                    continue;
+                }
+
+                bool overflow = false;
+                unsigned const digit = digit_value(ch);
+                std::uint64_t const old_value = result;
+
+                result *= base_;
+                overflow = result / base_ != old_value;
+
+                result += digit;
+                overflow |= result < digit;
+
+                if (overflow) {
+                    return std::nullopt;
+                }
+            }
+        }
+
+        if (result > max_value) {
+            return std::nullopt;
+        } else {
+            return negative_ ? -result : result;
+        }
+    }
+
+private:
+    /// The content of the integer constant being parsed.
+    std::string_view content_;
+    /// The maximum number of bits that can be used to represent the integer constant.
+    unsigned bits_;
+    /// Represents whether the integer constant is signed.
+    bool signedness_;
+    /// The current position of the character being processed in the source code file. This is used
+    /// to report diagnostics.
+    unsigned cur_pos_;
+    /// The sign of the integer constant. It can be either positive or negative.
+    bool negative_;
+    /// The base of the integer constant. Available values are 2, 8, 10, and 16.
+    unsigned base_;
+
+    static auto is_separator(char ch) -> bool {
+        return ch == '_';
+    }
+
+    /// Returns the max value of the integer constant based on the number of bits and signedness.
+    /// Note that we distinguish the valid range of positive and negative numbers. For negative
+    /// numbers, we return the valid range before negation.
+    auto get_max_value() const -> std::uint64_t {
+        if (negative_) {
+            // For negative integers, returns the available range of the value before being negated.
+            return static_cast<std::uint64_t>(1) << (bits_ - 1);
+        } else if (signedness_) {
+            // The range of signed integers is [-2^(bits - 1), 2^(bits - 1) - 1]. Returns the range
+            // of non-negative integers.
+            return (static_cast<std::uint64_t>(1) << (bits_ - 1)) - 1;
+        } else {
+            // The range of unsigned integers is [0, 2^bits - 1].
+            if (bits_ == 64) {
+                return std::numeric_limits<std::uint64_t>::max();
+            } else {
+                return (static_cast<std::uint64_t>(1) << bits_) - 1;
+            }
+        }
+    }
+
+    /// Returns whether the integer constant can always fit into 64 bits. If so, we can perform the
+    /// calculation without worrying about overflow.
+    auto always_fits_into_64bits() const -> bool {
+        std::size_t const digits = content_.size() - std::ranges::count(content_, '_');
+        switch (base_) {
+        case 2:
+            return digits <= 64;
+
+        case 8:
+            return digits <= 64 / 3;
+
+        case 10:
+            return digits <= 19;
+
+        case 16:
+            return digits <= 64 / 4;
+
+        default:
+            unreachable();
+        }
+    }
+
+    static auto digit_value(char ch) -> unsigned {
+        if ('0' <= ch && ch <= '9') {
+            return ch - '0';
+        } else if ('a' <= ch && ch <= 'f') {
+            return ch - 'a' + 10;
+        } else if ('A' <= ch && ch <= 'F') {
+            return ch - 'A' + 10;
+        } else {
+            unreachable();
+        }
+    }
+};
+}  // namespace
+
+auto Parser::get_integer_constant(Token const &token, unsigned bits, bool signedness)
+    -> std::optional<std::uint64_t>  //
+{
+    IntegerParser parser(token, bits, signedness);
+    Diag diag = create_diag_at_token(token, DiagLevel::Error, "Invalid integer constant");
+
+    parser.parse_sign();
+    parser.parse_base();
+
+    if (std::optional<IntegerParser::DiagMessage> note;
+        // NOLINTNEXTLINE(bugprone-assignment-in-if-condition)
+        (note = parser.check_separator()) || (note = parser.check_digit()))
+    {
+        string_pool_.push_back(std::move(note->message));
+        diagnostics_.push_back(
+            std::move(diag).with_sub_diag_entry(
+                ants::DiagEntry(DiagLevel::Note, string_pool_.back())
+                    .with_source(
+                        ants::AnnotatedSource(lexer_.source(), origin_)
+                            .with_primary_annotation(note->annotation_begin, note->annotation_end)
+                    )
+            )
+        );
+
+        return std::nullopt;
+    }
+
+    if (auto value = parser.parse_integer()) {
+        return value;
+    } else {
+        // The integer constant has overflowed. Generate diagnostic information.
+        if (signedness) {
+            std::int64_t const max = (static_cast<std::int64_t>(1) << (bits - 1)) - 1;
+            std::int64_t const min = -(max + 1);
+
+            string_pool_.push_back(fmt::format("the valid range is [{}, {}]", min, max));
+        } else {
+            string_pool_.push_back(
+                fmt::format(
+                    "the valid range is [0, {}]",
+                    bits == 64 ? std::numeric_limits<std::uint64_t>::max()
+                               : (static_cast<std::uint64_t>(1) << bits) - 1
+                )
+            );
+        }
+
+        diagnostics_.push_back(
+            std::move(diag)
+                .with_sub_diag_entry(DiagLevel::Note, "because the integer constant overflows")
+                .with_sub_diag_entry(DiagLevel::Note, string_pool_.back())
+        );
+        return std::nullopt;
+    }
 }
 
 auto Parser::expect_integer_constant(Token const &token, unsigned bits, bool signedness)
